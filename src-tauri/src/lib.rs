28c057b5b892149d1581990aef5849d6c1b273d2
Robert Kommeter <kommeterrobert@gmail.com>
2023-11-16 20:23:01 +0100

    Add connection to supabase

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,32 @@
+use postgrest::Postgrest;
+use dotenv::dotenv;
+use std::env;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
+async fn greet(name: String) -> String {
+    let supabase_url = env::var("SUPABASE_URL").expect("SUPABASE_URL not found");
+    let supabase_key = env::var("SUPABASE_KEY").expect("SUPABASE_KEY not found");
+
+    let client = Postgrest::new("https://gybhoyqqcrvxwbsqfzpi.supabase.co/rest/v1/")
+    .insert_header(
+        "apikey",
+        env::var("SUPABASE_KEY").unwrap());
+        let response = client
+            .from("duixue")
+            .select("*")
+            .execute()
+            .await
+            .expect("Failed to execute query");
+    
+    println!("test {:?}", response.url());
+    println!("Response: {:?}", response.text().await.unwrap());
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    dotenv().ok();
     tauri::Builder::default()
         .plugin(tauri_plugin_window::init())
         .plugin(tauri_plugin_shell::init())
--- src/App.svelte
+++ src/App.svelte
@@ -7,4 +7,5 @@
     w-96
   </div>
   <button class="bg-blue-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"></button>
+  <Greet />
 </main>
