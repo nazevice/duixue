251b474c04448b07c57d222848810e919b56faba
Robert Kommeter <kommeterrobert@gmail.com>
2023-11-18 23:11:34 +0100

    Add file upload to card

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,18 @@
 use postgrest::Postgrest;
 use dotenv::dotenv;
 use std::env;
+use base64::{Engine as _, engine::{self, general_purpose}};
 use serde::Deserialize;
+use reqwest::header::HeaderValue;
+use supabase_storage::Storage;
 
 #[derive(Debug, serde::Deserialize)]
 struct FormCard {
     character: String,
     pinyin: String,
     translation: String,
+    file_base64: String
 }
-
-let client = Postgrest::new("https://gybhoyqqcrvxwbsqfzpi.supabase.co/rest/v1/")
-    .insert_header(
-        "apikey",
-        env::var("SUPABASE_KEY").unwrap());
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -29,13 +28,25 @@ async fn greet(name: String) -> String {
             .await
             .expect("Failed to execute query");
     
-    println!("test {:?}", response.url());
+    println!("test {:?}", response);
     println!("Response: {:?}", response.text().await.unwrap());
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[tauri::command]
 async fn add_card(card: FormCard) -> String {
+    let file_data = general_purpose::STANDARD.decode(card.file_base64.split(',').last().unwrap()).expect("Failed to decode base64");
+    let client = reqwest::Client::new();
+    let response = client
+    .post("https://gybhoyqqcrvxwbsqfzpi.supabase.co/storage/v1/object/chinese/hsk1/test.jpg")
+    .header("Authorization", format!("Bearer {}", env::var("SUPABASE_KEY").unwrap()))
+    .header("Content-Type", "application/octet-stream")
+    .body(file_data)
+    .send()
+    .await
+    .expect("Failed to send request");
+
+    println!("{:?}", response);
     println!("test {:?}", card);
 
     format!("Received: {:?}", card)
--- src/lib/CardInput.svelte
+++ src/lib/CardInput.svelte
@@ -1,13 +1,27 @@
 <script>
     import { invoke } from "@tauri-apps/api/primitives"
+    
+    let file;
+
+    function readFileAsBase64() {
+    if (!file) return;   
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+    });
+}
 
     let formCard = {
     character: '',
     pinyin: '',
-    translation: ''
+    translation: '',
+    file_base64: ''
     };
 
     const addCard = async () => {
+        formCard.file_base64 = await readFileAsBase64(file);
         let greetMsg = await invoke("add_card", {card: formCard})
     }
 </script>
@@ -19,6 +33,8 @@
         <input class="border-2 border-indigo-500 w-full" bind:value={formCard.pinyin} />
         <h1>Übersetzung</h1>
         <input class="border-2 border-indigo-500 w-full" bind:value={formCard.translation} />
+        <label for="file">Upload your file</label>
+        <input type="file" on:change="{(e) => (file = e.target.files[0])}" />
         <button class="bg-indigo-500 p-2 rounded-lg w-full" type="submit">Add</button>
     </form>
 </div>
